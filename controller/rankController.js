6f309bc23b0c8a38f70b1454064760eebc85ff71
주용준 <>
2022-08-17 15:02:43 +0900

    랭킹

---
+++ controller/rankController.js
@@ -0,0 +1,45 @@
+"use strict";
+const jwtmiddle = require("../middleware/jwt");
+const rankDAO = require("../DAO/rankDAO");
+
+async function rank(req, res, next) {
+  const jwt_token = req.headers.jwt_token;
+  const type = req.query.type;
+  try {
+    if (jwt_token == undefined) throw "로그인 정보가 없습니다.";
+    const permission = await jwtmiddle.jwtCerti(jwt_token);
+    const ranking = await rankDAO.rank(type, permission.USER_ID);
+    res.json({
+      Message: "성공",
+      Data: ranking,
+    });
+  } catch (err) {
+    res.json({
+      Message: "실패",
+      Error_message: err,
+    });
+  }
+}
+
+async function myrank(req, res, next) {
+  const jwt_token = req.headers.jwt_token;
+  try {
+    if (jwt_token == undefined) throw "로그인 정보가 없습니다.";
+    const permission = await jwtmiddle.jwtCerti(jwt_token);
+    const myrank = await rankDAO.myrank(permission.USER_ID);
+    res.json({
+      Message: "성공",
+      Data: myrank,
+    });
+  } catch (err) {
+    res.json({
+      Message: "실패",
+      Error_message: err,
+    });
+  }
+}
+
+module.exports = {
+  rank,
+  myrank,
+};
