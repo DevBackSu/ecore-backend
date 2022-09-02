073c35985e8baa60901d4e6fc00ceb0e63806a83
안수민 <20201052@vision.hoseo.edu>
2022-09-02 13:25:47 +0900

    update : badgeDAO

---
+++ DAO/badgeDAO.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const db = require("../db/db_info");
+
+function registerBadge(){
+
+}
+
+function oneChallengeBadge(){}
+
+function tenChallengeBadge(){}
+
+function oneDailyBadge(){}
+
+function tenDailyBadge(){}
+
+module.exports = {
+    registerBadge,
+    oneChallengeBadge,
+    tenChallengeBadge,
+    oneDailyBadge,
+    tenDailyBadge,
+}
